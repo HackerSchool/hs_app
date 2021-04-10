dc9942b86ec065a9c46346983b2f007727844b4c
ms92512 <mafaldadmserafim@tecnico.ulisboa.pt>
2021-04-10 12:06:56 +0100

    Update connect.js
    Expanded connect.js file

--- backend/connect.js
+++ backend/connect.js
@@ -1,10 +1,10 @@
 
 module.exports = {
-    client: '',//mysql
+    client: 'mysql2',
     connection: {
-        host: '127.0.0.1',
-        port:,
-        user:,
-        database:,
+        host: process.env.DB_HOST || '127.0.0.1',
+        port:process.env.DB_PORT,
+        user:process.env.DB_USERNAME || 'root',
+        database:process.env.DB_NAME || '',
     },
 };
